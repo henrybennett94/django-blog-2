b92bdbff29bda9483d3a28b27732d99721bca710
Henry Bennet <hwbennett94@gmail.com>
2024-08-09 15:17:17 +0000

    Install summernote

--- blog/models.py
+++ blog/models.py
@@ -11,10 +11,20 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f"The title of this post is {self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=modles.CASCADE, related_name="comments")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["-created_on"]
     
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+
